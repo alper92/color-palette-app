2800ce6e124e01b62e56de84295f0a54da3838d1
alper92 <abc@example.com>
2023-07-31 19:28:17 +0200

    set global font-size

--- styles.js
+++ styles.js
@@ -1,22 +1,24 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 html,
 body {
-  padding: 0;
-  margin: 0;
+
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   background-color: #efefef;
+  font-size: 62.5%;
 }
 
 a {
   color: inherit;
   text-decoration: none;
-}
-
-* {
-  box-sizing: border-box;
 }
 
 `;
