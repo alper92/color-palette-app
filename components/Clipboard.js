c97132809eec45bacdece1f7baefc8ff64a2d5cc
alper92 <abc@example.com>
2023-08-01 21:23:53 +0200

    add copy button with toggle function

--- components/Clipboard.js
+++ components/Clipboard.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 
 export default function Clipboard({ theme }) {
+  const [toggle, setToggle] = useState(true);
+
   const clipboardValue = `:root {
   --primary-color: ${theme.primary};
   --secondary-color: ${theme.secondary};
@@ -9,13 +12,16 @@ export default function Clipboard({ theme }) {
 
   function handleClipboardValue() {
     navigator.clipboard.writeText(clipboardValue);
+    setToggle(!toggle);
   }
 
   return (
     <>
       <Board>
         {clipboardValue}
-        <Button onClick={handleClipboardValue}>COPY</Button>
+        <Button onClick={handleClipboardValue} theme={theme}>
+          {toggle ? "copy code" : "copied!"}
+        </Button>
       </Board>
     </>
   );
@@ -23,7 +29,7 @@ export default function Clipboard({ theme }) {
 
 const Board = styled.pre`
   padding: 0.5rem;
-  font-size: 0.5rem;
+  font-size: 1.2rem;
   font-style: italic;
   color: grey;
   background-color: rgb(40, 44, 52);
@@ -33,13 +39,14 @@ const Board = styled.pre`
 
 const Button = styled.button`
   position: absolute;
-  top: 0;
-  right: 0;
-  font-size: 0.4rem;
+  background-color: ${({ theme }) => theme.secondary};
+  top: 10px;
+  right: 10px;
+  font-size: 1rem;
   border: none;
   color: white;
   font-weight: bolder;
-  padding: 0.2rem;
-  background: transparent;
+  padding: 0.4rem;
+  border-radius: 0.5rem;
   cursor: pointer;
 `;
